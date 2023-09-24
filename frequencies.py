df45790d9a8eac3aa2c50f1cab14510c5ab5bb72
KonyAHS <hussain.ben_alshaikh@kcl.ac.uk>
2023-09-24 13:46:14 +0100

    Solution for excersise

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,14 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    
+    for value in items:
+        #converts value to Strings
+        val = str(value)
+        
+        #decides if to increment or add new element
+        if val in frequencies.keys():
+            frequencies[val] += 1
+        else: 
+            frequencies[val] = 1
     return frequencies
